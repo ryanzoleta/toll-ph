9c12034de649cb22c9f59a32524327a9474c8fe5
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-08-02 23:26:35 +0800

    buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
   }
 </script>
 
-<div class="mx-5 flex flex-col gap-5 sm:mx-auto sm:w-3/5 sm:pt-5 md:w-1/2 lg:w-2/5 xl:w-4/12">
+<div class="mx-5 flex flex-col gap-10 sm:mx-auto sm:w-3/5 sm:pt-5 md:w-1/2 lg:w-2/5 xl:w-4/12">
   <div class="flex place-content-between place-items-center">
     <h1 class="text-3xl font-bold text-slate-950 dark:text-slate-300">toll.ph</h1>
 
@@ -39,19 +39,27 @@
     </IconButton>
   </div>
 
-  <div class="flex flex-col gap-2">
-    <h3 class="font-bold text-slate-700 dark:text-slate-300">Origin</h3>
-    <input
-      type="text"
-      class="w-full rounded-md bg-gray-100 px-3 py-3 text-slate-700 outline-none dark:bg-gray-800 dark:text-slate-300 placeholder:dark:text-slate-600"
-      placeholder="Enter point of origin" />
+  <div class="flex flex-col gap-5">
+    <div class="flex flex-col gap-2">
+      <h3 class="font-bold text-slate-700 dark:text-slate-300">Origin</h3>
+      <input
+        type="text"
+        class="w-full rounded-md bg-gray-100 px-3 py-3 text-slate-700 outline-none dark:bg-gray-800 dark:text-slate-300 placeholder:dark:text-slate-600"
+        placeholder="Enter point of origin" />
+    </div>
+
+    <div class="flex flex-col gap-2">
+      <h3 class="font-bold text-slate-700 dark:text-slate-300">Destination</h3>
+      <input
+        type="text"
+        class="w-full rounded-md bg-gray-100 px-3 py-3 text-slate-700 outline-none dark:bg-gray-800 dark:text-slate-300 placeholder:dark:text-slate-600"
+        placeholder="Enter point of destination" />
+    </div>
   </div>
 
-  <div class="flex flex-col gap-2">
-    <h3 class="font-bold text-slate-700 dark:text-slate-300">Destination</h3>
-    <input
-      type="text"
-      class="w-full rounded-md bg-gray-100 px-3 py-3 text-slate-700 outline-none dark:bg-gray-800 dark:text-slate-300 placeholder:dark:text-slate-600"
-      placeholder="Enter point of destination" />
+  <div class="flex flex-col gap-3">
+    <button
+      class="rounded-md bg-green-300 py-3 font-bold text-green-800 transition duration-100 hover:bg-green-400 dark:bg-green-800 dark:text-green-200 dark:hover:bg-green-700"
+      >Calculate</button>
   </div>
 </div>
