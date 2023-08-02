97d433a70dff38bec6afd21a46243e656da9052b
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-08-02 23:07:53 +0800

    ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import '../app.css';
 </script>
 
-<div class="min-h-screen bg-slate-50 transition duration-200 dark:bg-slate-950">
+<div class="min-h-screen bg-slate-50 transition duration-200 dark:bg-gray-950">
   <slot />
 </div>
