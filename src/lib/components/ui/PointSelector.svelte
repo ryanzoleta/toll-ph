122c6a1f2ac52f194b2642f902b7bf522d518826
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-08-09 19:08:06 +0800

    select on hover

--- src/lib/components/ui/PointSelector.svelte
+++ src/lib/components/ui/PointSelector.svelte
@@ -4,21 +4,23 @@
   import { fade } from 'svelte/transition';
 
   export let points: Point[];
+  export let kind: 'ENTRY' | 'EXIT';
 
   let input = '';
   let displaySearchResults = false;
   let searchResults: Point[] = [];
-  let selectedPointId = 0;
+  let selectedIndex = -1;
 
   function matches(query: string, point: Point) {
     return (
-      point.name?.toUpperCase().startsWith(query.toUpperCase()) ||
-      (point.expresswayId?.toUpperCase().startsWith(query.toUpperCase()) && query.length > 1)
+      (point.name?.toUpperCase().startsWith(query.toUpperCase()) ||
+        (point.expresswayId?.toUpperCase().startsWith(query.toUpperCase()) && query.length > 1)) &&
+      ((kind === 'ENTRY' && point.entryable) || (kind === 'EXIT' && point.exitable))
     );
   }
 
   $: if (input) {
-    searchResults = points.filter((p) => matches(input, p) && p.entryable) ?? [];
+    searchResults = points.filter((p) => matches(input, p)) ?? [];
   } else {
     searchResults = [];
   }
@@ -27,6 +29,10 @@
     displaySearchResults = true;
   } else if (searchResults.length === 0) {
     displaySearchResults = false;
+  }
+
+  $: if (!displaySearchResults) {
+    selectedIndex = -1;
   }
 </script>
 
@@ -41,31 +47,40 @@
     }}
     on:blur={() => {
       displaySearchResults = false;
+    }}
+    on:keydown={(e) => {
+      if (e.key === 'ArrowUp') {
+        selectedIndex = selectedIndex - 1 === -1 ? 0 : selectedIndex - 1;
+        e.preventDefault();
+      } else if (e.key === 'ArrowDown') {
+        selectedIndex =
+          selectedIndex + 1 === searchResults.length ? selectedIndex : selectedIndex + 1;
+        e.preventDefault();
+      }
     }} />
 
   {#if displaySearchResults}
     <div
-      class="absolute top-full z-10 mt-1 flex w-full flex-col rounded-md bg-gray-200"
+      class="absolute top-full z-10 mt-1 flex w-full flex-col rounded-md border border-gray-300 bg-gray-100"
       transition:fade={{ duration: 50 }}>
       {#each searchResults as point, index}
-        <div
-          class="flex place-content-between place-items-center px-2 py-2 {point.id ===
-          selectedPointId
-            ? 'bg-gray-300'
+        <button
+          class="flex place-content-between place-items-center px-2 py-2 {index === selectedIndex
+            ? 'bg-gray-200'
             : ''}
         {index === 0 ? 'rounded-t-md' : ''}
         {index === searchResults.length - 1 ? 'rounded-b-md' : ''}"
           role="textbox"
           tabindex="0"
-          on:keydown={() => {
-            console.log('hey');
+          on:mouseenter={() => {
+            selectedIndex = index;
           }}>
           <div class="flex place-items-center gap-1">
             <p class="font-bold">{capitalize(point.name)}</p>
             <p class="text-sm font-extralight italic">{stringifyEnum(point.descriptor)}</p>
           </div>
           <p class="text-sm text-gray-500">{point.expresswayId}</p>
-        </div>
+        </button>
       {/each}
     </div>
   {/if}
