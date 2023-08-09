6891c2761d3420b05c49c5755b925751539b3616
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-08-09 17:49:02 +0800

    update schema

--- src/lib/data/schema.ts
+++ src/lib/data/schema.ts
@@ -1,28 +1,27 @@
 import { boolean, int, mysqlEnum, mysqlTable, primaryKey, varchar } from 'drizzle-orm/mysql-core';
 
 export const expressway = mysqlTable('expressway', {
-  id: int('id').primaryKey().autoincrement(),
-  abbr: varchar('abbr', { length: 100 }),
+  id: varchar('id', { length: 50 }).primaryKey(),
   name: varchar('name', { length: 500 })
 });
 
 export const point = mysqlTable('point', {
   id: int('id').primaryKey().autoincrement(),
   name: varchar('name', { length: 500 }),
-  descriptor: varchar('descriptor', { length: 500 }),
-  expresswayId: int('expresway_id').references(() => expressway.id),
-  paid: boolean('paid'),
-  entryable: boolean('entryable')
+  descriptor: mysqlEnum('descriptor', ['ENTRANCE_RAMP', 'EXIT_RAMP', 'TOLL_GATE']),
+  expresswayId: varchar('expresway_id', { length: 50 }).references(() => expressway.id),
+  entryable: boolean('entryable'),
+  exitable: boolean('exitable')
 });
 
 export const link = mysqlTable(
   'link',
   {
-    originId: int('origin_point_id').references(() => point.id),
-    nextId: int('next_point_id').references(() => point.id),
-    direction: mysqlEnum('direction', ['north', 'south'])
+    originPointId: int('origin_point_id').references(() => point.id),
+    nextPointId: int('next_point_id').references(() => point.id),
+    direction: mysqlEnum('direction', ['NORTH', 'SOUTH'])
   },
   (t) => ({
-    pk: primaryKey(t.originId, t.nextId, t.direction)
+    pk: primaryKey(t.originPointId, t.nextPointId, t.direction)
   })
 );
