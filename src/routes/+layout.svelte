5fa55fc91a05de4045b420b55f603dea029b6456
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-08-16 01:11:16 +0800

    add title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import '../app.css';
 </script>
 
+<svelte:head>
+  <title>Toll.ph - Toll Fee Calculator</title>
+</svelte:head>
+
 <div class="min-h-screen bg-slate-50 transition duration-200 dark:bg-gray-950">
   <slot />
 </div>
