9fe29e96ce9818387ac3ad52cb2e4e307242cc24
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-08-21 21:39:28 +0800

    use react query

--- src/lib/components/admin/Next.svelte
+++ src/lib/components/admin/Next.svelte
@@ -3,33 +3,49 @@
   import axios from 'axios';
   import IconPlus from '../icons/IconPlus.svelte';
   import IconTrash from '../icons/IconTrash.svelte';
+  import { createQuery } from '@tanstack/svelte-query';
 
   export let point: Point;
   export let direction: 'NORTH' | 'SOUTH';
 
-  let nextList = direction === 'NORTH' ? point.nextNorths : point.nextSouths;
   let destination: string;
+
+  const query = createQuery({
+    queryKey: ['next', point.id, direction],
+    queryFn: async () => {
+      const response = await axios.get(`/api/link/${point.id}/${direction}`);
+      return response.data;
+    },
+    refetchOnWindowFocus: false
+  });
+
+  $query;
 
   async function deleteLink(id: number, nextId: number, dir: 'NORTH' | 'SOUTH') {
     await axios.delete(`/api/link/${id}/${nextId}/${dir}`);
+    $query.refetch();
   }
 
   async function addLink() {
     await axios.post('/api/link', { originId: point.id, destination: destination, direction });
+    $query.refetch();
+    destination = '';
   }
 </script>
 
 <div>Next {direction}</div>
 <div>
-  {#if nextList}
-    <div class="flex place-items-center gap-2">
-      <input
-        type="text"
-        class="rounded-md border border-gray-800 bg-gray-800 p-1"
-        bind:value={destination} />
-      <button class="h-5 w-5 text-gray-500" on:click={addLink}><IconPlus /></button>
-    </div>
-    {#each nextList as next}
+  <div class="flex place-items-center gap-2">
+    <input
+      type="text"
+      class="rounded-md border border-gray-800 bg-gray-800 p-1"
+      bind:value={destination} />
+    <button class="h-5 w-5 text-gray-500" on:click={addLink}><IconPlus /></button>
+  </div>
+  {#if $query.isLoading}
+    <p>Loading...</p>
+  {:else}
+    {#each $query.data as next}
       <div class="flex place-content-between">
         <p>{next.name}</p>
         <button
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script>
   import '../app.css';
+  import { browser } from '$app/environment';
+  import { QueryClientProvider, QueryClient } from '@tanstack/svelte-query';
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        enabled: browser
+      }
+    }
+  });
 </script>
 
 <svelte:head>
   <title>Toll.ph - Toll Fee Calculator</title>
 </svelte:head>
 
-<div class="min-h-screen bg-slate-50 transition duration-200 dark:bg-gray-950">
-  <slot />
-</div>
+<QueryClientProvider client={queryClient}>
+  <div class="min-h-screen bg-slate-50 transition duration-200 dark:bg-gray-950">
+    <slot />
+  </div>
+</QueryClientProvider>
