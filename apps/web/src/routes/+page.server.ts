cffb99c7b7eceeb936b4065cf024d457330fb5d9
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-06-21 20:13:51 +0800

    basic toll fee calculation

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,7 +1,6 @@
 import { db } from '$lib/data/db';
 import type { Point } from '$lib/data/schema';
 import { expressway, point, tollMatrix, tollNetwork } from '$lib/data/schema';
-import type { TollFeeMatrix } from '$lib/types';
 import { eq } from 'drizzle-orm';
 
 export async function load() {
@@ -18,27 +17,10 @@ export async function load() {
     .innerJoin(tollNetwork, eq(tollNetwork.id, expressway.tollNetworkId));
 
   const matrix = await db.select().from(tollMatrix);
-  const tollFeeMatrix: TollFeeMatrix[] = [];
-
-  for (const t of matrix) {
-    let determinantList = [];
-
-    determinantList.push(t.entryPointId);
-    if (t.entryPointId !== t.exitPointId) determinantList.push(t.exitPointId);
-    if (t.entryPointId !== 99999 && t.exitPointId !== 99999)
-      determinantList = determinantList.sort();
-
-    const determinants = determinantList.join(',');
-
-    tollFeeMatrix.push({
-      determinants: determinants,
-      fee: parseFloat(t.fee as string),
-    });
-  }
 
   return {
     points: points,
     expressways,
-    tollFeeMatrix,
+    tollMatrix: matrix,
   };
 }
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import PointSelector from '$lib/components/ui/PointSelector.svelte';
-  import type { Action, Point } from '$lib/types';
-  import { capitalize, formatAmountToCurrency, stringifyEnum } from '$lib/utils.js';
-  import { expressways, points, tollFeeMatrix } from '$lib/stores.js';
+  import type { Point } from '$lib/types';
+  import { formatAmountToCurrency } from '$lib/utils.js';
+  import { expressways, points } from '$lib/stores.js';
   import Button from '$lib/components/ui/button/button.svelte';
   import { Sun, Moon } from 'lucide-svelte';
   import { toggleMode } from 'mode-watcher';
@@ -11,11 +11,9 @@
 
   $points = data.points;
   $expressways = data.expressways;
-  $tollFeeMatrix = data.tollFeeMatrix;
 
   let pointOrigin: Point | null = null;
   let pointDestination: Point | null = null;
-  let actions: Action[] = [];
   let tollFee = 0;
 
   let pointOriginInput = '';
@@ -23,16 +21,11 @@
 
   async function calculate() {
     if (pointOrigin && pointDestination) {
-      tollFee = 0;
-
-      actions.forEach((a) => {
-        if (a.amount) tollFee += a.amount;
-      });
+      const matrix = data.tollMatrix.find(
+        (tm) => tm.entryPointId === pointOrigin?.id && tm.exitPointId === pointDestination?.id
+      );
+      tollFee = matrix ? parseFloat(matrix.fee ?? '0') : 0;
     }
-  }
-
-  function getExpressway(point: Point) {
-    return $expressways.find((e) => e.id === point.expresswayId);
   }
 </script>
 
@@ -80,90 +73,21 @@
         pointDestination = null;
         pointOriginInput = '';
         pointDestinationInput = '';
-        actions = [];
       }}>Clear</button>
   </div>
 
-  {#if actions.length > 0}
+  {#if tollFee > 0}
     <div class="flex flex-col gap-4">
       <div
         class="flex flex-col gap-2 rounded-lg bg-gray-200 p-5 dark:bg-gray-800 dark:text-gray-200">
-        <div class="flex place-content-between">
-          <h2 class="text-xl font-bold text-gray-500">Total Toll Fees</h2>
+        <div class="flex flex-col">
+          <h2 class="text-gray-500">Total Toll Fees</h2>
           <div class="flex flex-col gap-5">
             <p class="text-5xl font-extrabold tracking-tight text-gray-900 dark:text-gray-200">
               {formatAmountToCurrency(tollFee)}
             </p>
           </div>
         </div>
-        <div>
-          <h2 class=" text-xl font-bold text-gray-500">Pay at these toll gates</h2>
-          <div>
-            {#each actions as action}
-              {#if action.amount}
-                <div class="flex place-content-between">
-                  <p>
-                    <span class="font-bold">{capitalize(action.point.name)}</span>
-                    <span class="text-gray-500"
-                      >({stringifyEnum(getExpressway(action.point)?.rfid)})</span>
-                  </p>
-                  <p class="font-bold tracking-tight text-gray-600 dark:text-gray-200">
-                    {formatAmountToCurrency(action.amount)}
-                  </p>
-                </div>
-              {/if}
-            {/each}
-          </div>
-        </div>
-      </div>
-
-      <div class="flex flex-col rounded-lg bg-gray-200 p-5 dark:bg-gray-800 dark:text-gray-200">
-        <h2 class="mb-2 text-xl font-bold text-gray-500">Route</h2>
-        {#each actions as action, index}
-          <div class="flex gap-2 {index === 0 ? 'place-items-end' : 'place-items-start'}">
-            <div class="relative flex h-5 w-5 flex-col place-items-center">
-              {#if index === 0}
-                <div class="absolute top-1 h-full w-2 bg-blue-400" />
-                <div
-                  class="absolute left-0 top-0 h-full w-full rounded-full border-4 border-blue-400 bg-white" />
-              {:else if index === actions.length - 1}
-                <div class="absolute bottom-1 h-full w-2 bg-blue-400" />
-                <div
-                  class="absolute bottom-0 left-0 h-full w-full rounded-full border-4 border-blue-400 bg-white" />
-              {:else}
-                <div class="absolute top-1 h-full w-2 bg-blue-400" />
-                <div
-                  class="absolute left-0 top-0 h-full w-full rounded-full border-4 border-blue-400 bg-white" />
-              {/if}
-            </div>
-            {#if action.action === 'PAY' && action.amount}
-              <p class="text-gray-700 dark:text-gray-400">
-                {capitalize(action.action.toLowerCase())}
-                <span class="font-bold text-gray-800 dark:text-gray-100"
-                  >{formatAmountToCurrency(action.amount)}</span>
-                toll fee at
-                <span class="font-bold text-gray-800 dark:text-gray-100"
-                  >{capitalize(action.point.name)}</span>
-                <span class="text-gray-500"
-                  >({stringifyEnum(getExpressway(action.point)?.rfid)})</span>
-              </p>
-            {:else if action.action === 'ENTER' || action.action === 'EXIT'}
-              <p class="text-gray-700 dark:text-gray-400">
-                {capitalize(action.action.toLowerCase())}
-                {action.point.expresswayId}
-                at
-                <span class="font-bold text-gray-800 dark:text-gray-100"
-                  >{capitalize(action.point.name)}</span>
-              </p>
-            {/if}
-          </div>
-
-          {#if index !== actions.length - 1}
-            <div class="flex h-5 w-5 flex-col place-items-center">
-              <div class="h-full w-2 bg-blue-400" />
-            </div>
-          {/if}
-        {/each}
       </div>
     </div>
   {/if}
