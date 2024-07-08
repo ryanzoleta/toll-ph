2200aa953738b6f0dfa0b1b2177acf9cecedde1f
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-07-08 19:18:17 +0800

    fix build issue

--- apps/web/src/lib/data/db.ts
+++ apps/web/src/lib/data/db.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import pg from 'pg';
 import * as schema from './schema';
 
-export const connection = new Pool({
+export const connection = new pg.Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
