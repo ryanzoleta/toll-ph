401d4f599d69d072666511da244690b18e8e6b7c
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-07-08 17:31:54 +0800

    show message if origin not selected

--- apps/web/src/lib/components/ui/PointSelector.svelte
+++ apps/web/src/lib/components/ui/PointSelector.svelte
@@ -135,5 +135,17 @@
         </button>
       {/each}
     </div>
+  {:else if displaySearchResults && searchResults.length === 0}
+    <div
+      class="absolute top-full z-10 mt-1 flex max-h-[500px] w-full flex-col overflow-scroll rounded-md border border-gray-300 bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100"
+      transition:fade={{ duration: 50 }}>
+      <button
+        class="flex place-content-between place-items-center bg-gray-200 px-2
+          py-2 dark:bg-gray-900">
+        <div class="flex place-items-center gap-2">
+          <p class="italic">Pick a point of origin</p>
+        </div>
+      </button>
+    </div>
   {/if}
 </div>
