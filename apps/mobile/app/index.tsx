064ea36d0e115c4807884669324fa8ac04add185
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-07-12 20:45:44 +0800

    setup nativewind

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView, Text } from 'react-native';
 function index() {
   return (
     <SafeAreaView>
-      <Text style={{ color: 'white' }}>index</Text>
+      <Text className="text-2xl font-bold text-white underline">index</Text>
       <Link href="/matrix" style={{ color: 'white', fontSize: 50 }}>
         matrix page
       </Link>
