6c416b314c95b8e87396e7376a8b31f6b5380d12
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-07-14 21:27:29 +0800

    improve dark mode button styling

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'expo-router';
 import { Moon, Sun } from 'lucide-react-native';
 import { Pressable, SafeAreaView, Text, View } from 'react-native';
 import { useColorScheme } from 'nativewind';
+import colors from 'tailwindcss/colors';
 
 function index() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
@@ -9,13 +10,18 @@ function index() {
   return (
     <SafeAreaView className="bg-background min-h-screen">
       <View className="px-5 py-2">
-        <View className="flex flex-row justify-between">
-          <Text className="text-foreground text-4xl font-extrabold tracking-tight underline">
-            toll.ph
-          </Text>
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-foreground text-4xl font-extrabold tracking-tight">toll.ph</Text>
 
-          <Pressable onPress={toggleColorScheme}>
-            {colorScheme === 'dark' ? <Sun /> : <Moon />}
+          <Pressable
+            onPressIn={toggleColorScheme}
+            className="transition-opacity duration-100 active:opacity-50"
+          >
+            {colorScheme === 'dark' ? (
+              <Sun color={colors.slate[500]} />
+            ) : (
+              <Moon color={colors.slate[500]} />
+            )}
           </Pressable>
         </View>
         <Link href="/matrix" className="text-foreground">
