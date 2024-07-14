1972da24d63fc912ea786b8d824bc21a6770acc4
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-07-14 21:42:49 +0800

    ui

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -9,7 +9,7 @@ function index() {
 
   return (
     <SafeAreaView className="bg-background min-h-screen">
-      <View className="px-5 py-2">
+      <View className="flex flex-col gap-3 px-5 py-2">
         <View className="flex flex-row items-center justify-between">
           <Text className="text-foreground text-4xl font-extrabold tracking-tight">toll.ph</Text>
 
@@ -33,9 +33,28 @@ function index() {
             </Pressable>
           </View>
         </View>
+
+        <View>
+          <Text className="text-muted">
+            Use this to calculate the toll fee for a trip from anywhere in the entire Luzon
+            expressway network, form Baguio, to Manila, to Batangas
+          </Text>
+        </View>
+
+        <View>
+          <View>
+            <FormLabel>Vehicle Class</FormLabel>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
+}
+
+import { ReactNode } from 'react';
+
+function FormLabel({ children }: { children: ReactNode }) {
+  return <Text className="text-foreground text-lg font-bold">{children}</Text>;
 }
 
 export default index;
