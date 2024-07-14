617b97e89fb8e6ad11a82d7409d85a35b46a0e5d
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-07-14 22:01:12 +0800

    initial point selector

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,4 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -9,7 +8,6 @@ import '../global.css';
 
 // import { useColorScheme } from '@/components/useColorScheme';
 
-import { useColorScheme } from 'nativewind';
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -48,8 +46,6 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const { colorScheme } = useColorScheme();
-
   return (
     // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
     <Stack screenOptions={{ headerShown: false }}></Stack>
--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,11 +1,32 @@
 import { Link } from 'expo-router';
-import { Moon, Sun } from 'lucide-react-native';
+import { ChevronDown, Moon, Sun } from 'lucide-react-native';
 import { Pressable, SafeAreaView, Text, View } from 'react-native';
 import { useColorScheme } from 'nativewind';
 import colors from 'tailwindcss/colors';
 
 function index() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
+
+  const points = [
+    {
+      id: 1,
+      name: 'Magallanes',
+      expresswayId: 'SLEX',
+      sequence: 1,
+    },
+    {
+      id: 2,
+      name: 'Merville',
+      expresswayId: 'SLEX',
+      sequence: 2,
+    },
+    {
+      id: 3,
+      name: 'Bicutan',
+      expresswayId: 'SLEX',
+      sequence: 3,
+    },
+  ];
 
   return (
     <SafeAreaView className="bg-background min-h-screen">
@@ -42,8 +63,22 @@ function index() {
         </View>
 
         <View>
-          <View>
+          <View className="flex flex-col gap-1">
             <FormLabel>Vehicle Class</FormLabel>
+
+            <Pressable className="flex flex-row items-center justify-between rounded-md bg-slate-700 p-3">
+              <Text className="text-foreground text-lg font-bold">Class 1</Text>
+
+              <ChevronDown color={colors.slate[500]} size={20} />
+            </Pressable>
+          </View>
+        </View>
+
+        <View>
+          <View className="flex flex-col gap-1">
+            <FormLabel>Origin</FormLabel>
+
+            <PointSelector points={points} />
           </View>
         </View>
       </View>
@@ -51,10 +86,49 @@ function index() {
   );
 }
 
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 function FormLabel({ children }: { children: ReactNode }) {
   return <Text className="text-foreground text-lg font-bold">{children}</Text>;
+}
+
+function PointSelector({ points }) {
+  const [point, setPoint] = useState<string | null>(null);
+  const [setselecting, setSetselecting] = useState(false);
+
+  return (
+    <Pressable
+      className={twMerge(
+        'relative flex flex-row items-center justify-between rounded-md p-3 ',
+        point ? 'bg-slate-700' : 'bg-slate-800'
+      )}
+      onPress={() => setSetselecting(!setselecting)}
+    >
+      {point ? (
+        <Text className="text-foreground text-lg font-bold">{point}</Text>
+      ) : (
+        <Text className="text-muted text-lg">Select a point</Text>
+      )}
+
+      {setselecting && (
+        <View className="absolute left-0 top-full z-10 mt-1 w-full rounded-md bg-slate-800">
+          {points.map((point) => (
+            <Pressable
+              key={point.id}
+              onPress={() => {
+                setPoint(point.name);
+                setSetselecting(false);
+              }}
+              className="rounded-md p-2"
+            >
+              <Text className="text-foreground text-lg font-bold">{point.name}</Text>
+            </Pressable>
+          ))}
+        </View>
+      )}
+    </Pressable>
+  );
 }
 
 export default index;
