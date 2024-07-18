a3c74c3b3734893e1b0d0d20387e3aa63efab898
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-07-18 21:13:21 +0800

    text search

--- apps/mobile/app/selector/index.tsx
+++ apps/mobile/app/selector/index.tsx
@@ -1,11 +1,21 @@
 import { SafeAreaView, View, Text, TextInput, ScrollView, Pressable } from 'react-native';
 import { router, useLocalSearchParams } from 'expo-router';
 import { useAllPointsStore, useOriginStore } from '@/lib/stores';
+import { useEffect, useState } from 'react';
 
 export default function Selector() {
   const params = useLocalSearchParams();
   const { allPoints } = useAllPointsStore();
   const { setOrigin } = useOriginStore();
+
+  const [input, setInput] = useState('');
+  const [filteredPoints, setFilteredPoints] = useState(allPoints);
+
+  useEffect(() => {
+    setFilteredPoints(
+      allPoints.filter((point) => point.name.toLowerCase().includes(input.toLowerCase()))
+    );
+  }, [input]);
 
   return (
     <SafeAreaView className="bg-background min-h-screen">
@@ -16,12 +26,14 @@ export default function Selector() {
           className="bg-secondary text-secondary-foreground m-3 rounded-lg p-3 align-top"
           textAlignVertical="center"
           placeholder="Search for exit name or expressway"
+          value={input}
+          onChangeText={(text) => setInput(text)}
         />
       </View>
 
       <View className="flex flex-1 flex-col pb-16 pt-5">
         <ScrollView>
-          {allPoints.map((point) => (
+          {filteredPoints.map((point) => (
             <Pressable
               className="border-t-secondary flex flex-row items-center justify-between border-t p-5"
               key={point.id}
