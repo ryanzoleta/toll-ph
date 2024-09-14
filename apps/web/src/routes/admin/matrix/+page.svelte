19e681058dc936b7ed4fa88485e6fb60bcbd942a
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-09-14 23:47:27 +0800

    fix hiding of admin

--- apps/web/src/routes/admin/matrix/+page.svelte
+++ apps/web/src/routes/admin/matrix/+page.svelte
@@ -1,3 +1,13 @@
+<script context="module">
+  import { env } from '$env/dynamic/private';
+
+  export const prerender = true;
+
+  if (env.ENV !== 'development') {
+    throw new Error('This route is not available in production.');
+  }
+</script>
+
 <div>
   <h1>This is the admin page</h1>
 </div>
