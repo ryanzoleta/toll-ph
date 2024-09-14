c319b4c39daff410a11b2b169daef1734eb9ea7f
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-09-14 23:54:02 +0800

    admin

--- apps/web/src/routes/admin/matrix/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { dev } from '$app/environment';
-
-export const prerender = true;
-
-export async function load() {
-  if (!dev) {
-    throw new Error('This route is not available in production.');
-  }
-}
--- apps/web/src/routes/admin/matrix/+page.svelte
+++ apps/web/src/routes/admin/matrix/+page.svelte
@@ -1,3 +1,13 @@
+<script context="module">
+  import { dev } from '$app/environment';
+
+  export const prerender = true;
+
+  if (!dev) {
+    throw new Error('This route is not available in production.');
+  }
+</script>
+
 <div>
   <h1>This is the admin page</h1>
 </div>
