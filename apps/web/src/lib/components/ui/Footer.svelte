8da89cdaf584bfe3e2946163664755ebfbe74ce3
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-09-21 18:50:14 +0800

    ui

--- apps/web/src/lib/components/ui/Footer.svelte
+++ apps/web/src/lib/components/ui/Footer.svelte
@@ -1,11 +1,10 @@
 <div class="flex flex-col justify-between gap-5 p-5 pt-10 md:flex-row">
   <div class="flex flex-row justify-center gap-5 md:justify-start">
-    <p class="text-xs text-slate-500">
+    <p class="text-center text-xs text-slate-500 md:text-left">
       Source: <a
         href="https://trb.gov.ph/"
         target="_blank"
-        class="text-center text-xs text-slate-500 hover:underline md:text-left"
-        >Toll Regulatory Board</a>
+        class="text-xs text-slate-500 hover:underline">Toll Regulatory Board</a>
     </p>
 
     <p class="text-center text-xs text-slate-500 md:text-left">Last updated: September 15, 2024</p>
