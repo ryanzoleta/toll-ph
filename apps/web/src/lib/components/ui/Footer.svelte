92e2e516e651f6ab5731963c781a85918247be98
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-10-26 20:37:52 +0800

    donation links in footer

--- apps/web/src/lib/components/ui/Footer.svelte
+++ apps/web/src/lib/components/ui/Footer.svelte
@@ -12,9 +12,13 @@
 
   <div class="flex flex-row justify-center gap-5 md:justify-start">
     <a
+      href="https://www.buymeacoffee.com/ryanarnold"
+      target="_blank"
+      class="text-xs text-slate-500 hover:underline">Buy me a kape</a>
+    <a
       href="https://forms.gle/BG6voJJ2ZLtZdcmVA"
       target="_blank"
-      class="text-xs text-slate-500 hover:underline">Send Feedback</a>
+      class="text-xs text-slate-500 hover:underline">Report a bug</a>
     <a href="/support" class="text-xs text-slate-500 hover:underline">Support</a>
     <a href="/privacy" class="text-xs text-slate-500 hover:underline">Privacy</a>
     <a href="/terms" class="text-xs text-slate-500 hover:underline">Terms</a>
