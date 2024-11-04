9c8b482d76f559bbfddb5079816d00d9f9b1a29c
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-11-04 18:19:38 +0800

    remove admin folder

--- apps/admin/src/lib/stores.ts
+++
@@ -1,8 +0,0 @@
-import type { TollFeeMatrix, Expressway } from '$lib/types';
-import { writable } from 'svelte/store';
-// import type { Expressway } from './data/schema';
-import type { Point } from '$lib/data/schema';
-
-export const points = writable<Point[]>([]);
-export const expressways = writable<Expressway[]>([]);
-export const tollFeeMatrix = writable<TollFeeMatrix[]>([]);
