22aaaf9786729346310eee1933190c820b3d09ff
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-12-14 14:24:00 +0800

    get expressway

--- apps/web/src/routes/api/expressway/+server.ts
+++ apps/web/src/routes/api/expressway/+server.ts
@@ -1,9 +1,17 @@
 import { error, RequestEvent } from '@sveltejs/kit';
 import { API_KEY } from '$env/static/private';
 import { verifyApiKey } from '../utils';
+import { db } from '$lib/data/db';
+import { expressway } from '$lib/data/schema';
 
 export async function GET(event: RequestEvent) {
   verifyApiKey(event);
 
-  return Response.json({ message: 'Hello from Expressway!' });
+  const expressways = await db.select().from(expressway);
+
+  if (expressways) {
+    return Response.json(expressways);
+  }
+
+  return error(404, 'Not Found');
 }
